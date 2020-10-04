60ab87b6cd181313274fa9b0f8bd1bbcbab9e6c5
Ryan Cahoon <ryan.m.cahoon@gmail.com>
2020-10-04 20:28:19 +0000

    Launcher tutorial code

--- src/main/java/com/team766/robot/Robot.java
+++ src/main/java/com/team766/robot/Robot.java
@@ -5,9 +5,11 @@ import com.team766.robot.mechanisms.*;
 public class Robot {
 	// Declare mechanisms here
 	public static Drive drive;
+	public static Launcher launcher;
 
 	public static void robotInit() {
 		// Initialize mechanisms here
 		drive = new Drive();
+		launcher = new Launcher();
 	}
 }
