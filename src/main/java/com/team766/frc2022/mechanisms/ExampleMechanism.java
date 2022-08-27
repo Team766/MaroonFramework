84a783e334d87ce3cde9050dfc85ec34b5065d83
Jayen Agrawal <69493224+JayAgra@users.noreply.github.com>
2022-08-27 12:18:12 -0700

    First  steps for CANdle

--- src/main/java/com/team766/frc2022/mechanisms/ExampleMechanism.java
+++ src/main/java/com/team766/frc2022/mechanisms/ExampleMechanism.java
@@ -4,19 +4,24 @@ import com.team766.framework.Mechanism;
 import com.team766.hal.RobotProvider;
 import com.team766.hal.SpeedController;
 
+import com.ctre.phoenix.led.CANdle;
+
+
 public class ExampleMechanism extends Mechanism {
-	private SpeedController m_leftMotor;
-	private SpeedController m_rightMotor;
+	//private SpeedController m_leftMotor;
+	//private SpeedController m_rightMotor;
+	private CANdle candle;
 
 	public ExampleMechanism() {
-		m_leftMotor = RobotProvider.instance.getCANMotor("exampleMechanism.leftMotor");
-		m_rightMotor = RobotProvider.instance.getCANMotor("exampleMechanism.rightMotor");
+		//m_leftMotor = RobotProvider.instance.getCANMotor("exampleMechanism.leftMotor");
+		//m_rightMotor = RobotProvider.instance.getCANMotor("exampleMechanism.rightMotor");
+		candle = new CANdle(0); //ID is 0 right now
 	}
 
-	public void setMotorPower(double leftPower, double rightPower){
+	public void setCANdle(int R, int G, int B){
 		checkContextOwnership();
-
-		m_leftMotor.set(leftPower);
-		m_rightMotor.set(rightPower);
+		candle.setLEDs(R, G, B);
+		//m_leftMotor.set(leftPower);
+		//m_rightMotor.set(rightPower);
 	}
 }
