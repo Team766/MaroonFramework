Team766/2022

Plotting from logged values

src/main/java/com/team766/web/AutonomousSelector.java
@@ -62,7 +62,7 @@ public class AutonomousSelector implements WebServer.Handler {
 			"<h1>Autonomous Mode Selector</h1>",
 			"<h3 id=\"current-mode\">Current Mode: " + String.valueOf(autonMode) + "</h1>",
 			"<form>",
-			"<p>" + HtmlElements.buildDropDown("AutoMode", autonMode, AUTONS) + "</p>",
+			"<p>" + HtmlElements.buildDropDown("AutoMode", autonMode, Arrays.stream(AUTONS)::iterator) + "</p>",
 			"<input type=\"submit\" value=\"Submit\"></form>",
 			"<script>",
 			"  function refreshAutoMode() {",

Team766/MaroonFramework

src/main/java/com/team766/web/AutonomousSelector.java
@@ -67,7 +67,7 @@ public class AutonomousSelector implements WebServer.Handler {
 			"<h1>Autonomous Mode Selector</h1>",
 			"<h3 id=\"current-mode\">Current Mode: " + selectedAutonModeName + "</h1>",
 			"<form>",
-			"<p>" + HtmlElements.buildDropDown("AutoMode", selectedAutonModeName, m_autonModeNames) + "</p>",
+			"<p>" + HtmlElements.buildDropDown("AutoMode", selectedAutonModeName, Arrays.stream(m_autonModeNames)::iterator) + "</p>",
 			"<input type=\"submit\" value=\"Submit\"></form>",
 			"<script>",
 			"  function refreshAutoMode() {",