d16b12c66ac569358c99656a1efc5ca4e4b6b1e3
Max <68516760+TTVMixmix00@users.noreply.github.com>
2023-04-26 18:42:27 -0700

    i want to take a break

--- src/main/java/com/team766/controllers/PID.java
+++ src/main/java/com/team766/controllers/PID.java
@@ -20,179 +20,122 @@ import com.revrobotics.CANSparkMax;
 import com.team766.framework.Mechanism;
 import com.team766.hal.MotorController;
 import com.team766.library.RateLimiter;
+import com.team766.controllers.PIDException;
 
 public class PID {
 	private MotorController mc1; 
     private CANSparkMax csm1;
     private SparkMaxPIDController pid1;
     private SparkMaxAbsoluteEncoder abs1;
-	public static double dz1; 
-	public static double maxpos1;
-	public static double minpos1;
-	public static double maxvel1;
-	public static double maxaccel1;
-	public static double maxspeed1;
-	public static double minspeed1;
+	public static double dz1 = 0; 
+	public static double maxpos1 = 0;
+	public static double minpos1 = 0;
+	public static double maxvel1 = 0;
+	public static double maxaccel1 = 0;
+	public static double maxspeed1 = 0;
+	public static double minspeed1 = 0;
 
-	private MotorController mc2; 
-    private CANSparkMax csm2; 
-    private SparkMaxPIDController pid2;
-    private SparkMaxAbsoluteEncoder abs2;
-	public static double dz2;
-	public static double maxpos2;
-	public static double minpos2;
-	public static double maxvel2;
-	public static double maxaccel2;
-	public static double maxspeed2;
-	public static double minspeed2;
+	private enum PIDSTATE{
+		PID,
+		OFF,
+		ANTIGRAV
+	}
 
-	private MotorController mc3;
-    private CANSparkMax csm3;
-    private SparkMaxPIDController pid3;
-    private SparkMaxAbsoluteEncoder abs3;
-	public static double dz3;
-	public static double maxpos3;
-	public static double minpos3;
-	public static double maxvel3;
-	public static double maxaccel3;
-	public static double maxspeed3;
-	public static double minspeed3;
-
-	private MotorController mc4;
-    private CANSparkMax csm4;
-    private SparkMaxPIDController pid4;
-    private SparkMaxAbsoluteEncoder abs4;
-	public static double dz4;
-	public static double maxpos4;
-	public static double minpos4;
-	public static double maxvel4;
-	public static double maxaccel4;
-	public static double maxspeed4;
-	public static double minspeed4;
-
-	private MotorController mc5;
-    private CANSparkMax csm5; 
-    private SparkMaxPIDController pid5;  
-    private SparkMaxAbsoluteEncoder abs5; 
-	public static double dz5;
-	public static double maxpos5;
-	public static double minpos5;
-	public static double maxvel5;
-	public static double maxaccel5;
-	public static double maxspeed5;
-	public static double minspeed5;
+	private PIDSTATE theState = PIDSTATE.OFF;
 
 
-	public PID(int numMotors){
-		if(numMotors >5){
-			Logger.get(Category.CONTROL).log(Severity.ERROR, "PID: Too many motors. Please add more manually to PID.java");
-		} else if(numMotors < 1){
-			Logger.get(Category.CONTROL).log(Severity.ERROR, "PID: how u gonna make a pid with no motors just stop");
-		} else if(numMotors > 4){
-			mc5 = RobotProvider.instance.getMotor("pid5");
-			csm5 = (CANSparkMax)mc5;
-			pid5  = csm5.getPIDController();
-		} else if(numMotors > 3){
-			mc5 = RobotProvider.instance.getMotor("pid5");
-			csm5 = (CANSparkMax)mc5;
-			pid5  = csm5.getPIDController();
-			mc4 = RobotProvider.instance.getMotor("pid4");
-			csm4 = (CANSparkMax)mc4;
-			pid4  = csm4.getPIDController();
-		} else if(numMotors > 2){
-			mc5 = RobotProvider.instance.getMotor("pid5");
-			csm5 = (CANSparkMax)mc5;
-			pid5  = csm5.getPIDController();
-			mc4 = RobotProvider.instance.getMotor("pid4");
-			csm4 = (CANSparkMax)mc4;
-			pid4  = csm4.getPIDController();
-			mc3 = RobotProvider.instance.getMotor("pid3");
-			csm3 = (CANSparkMax)mc3;
-			pid3  = csm3.getPIDController();
-		} else if(numMotors > 1){
-			mc5 = RobotProvider.instance.getMotor("pid5");
-			csm5 = (CANSparkMax)mc5;
-			pid5  = csm5.getPIDController();
-			mc4 = RobotProvider.instance.getMotor("pid4");
-			csm4 = (CANSparkMax)mc4;
-			pid4  = csm4.getPIDController();
-			mc3 = RobotProvider.instance.getMotor("pid3");
-			csm3 = (CANSparkMax)mc3;
-			pid3  = csm3.getPIDController();
-			mc2 = RobotProvider.instance.getMotor("pid2");
-			csm2 = (CANSparkMax)mc2;
-			pid2 = csm2.getPIDController();
-		} else if(numMotors > 0){
-			mc5 = RobotProvider.instance.getMotor("pid5");
-			csm5 = (CANSparkMax)mc5;
-			pid5  = csm5.getPIDController();
-			mc4 = RobotProvider.instance.getMotor("pid4");
-			csm4 = (CANSparkMax)mc4;
-			pid4  = csm4.getPIDController();
-			mc3 = RobotProvider.instance.getMotor("pid3");
-			csm3 = (CANSparkMax)mc3;
-			pid3  = csm3.getPIDController();
-			mc2 = RobotProvider.instance.getMotor("pid2");
-			csm2 = (CANSparkMax)mc2;
-			pid2 = csm2.getPIDController();
-			mc1 = RobotProvider.instance.getMotor("pid1");
+	public PID(String configName){
+			mc1 = RobotProvider.instance.getMotor(configName);
 			csm1 = (CANSparkMax)mc1;
 			pid1 = csm1.getPIDController();
-		}
+		
 	}
-}
-/* 
-} else if(numMotors > 4){
-	mc5.setNeutralMode(NeutralMode.Brake);
-	mc5.setInverted(true);
-	pid5.setP(0.0001);
-	pid5.setI(0);
-	pid5.setD(0);
-	pid5.setIZone(0);
-	pid5.setFF(0);
-	pid5.setOutputRange(min5, max5);
-	abs5.setPosition(0);
-} else if(numMotors > 3){
-	mc4.setNeutralMode(NeutralMode.Brake);
-	mc4.setInverted(true);
-	pid4.setP(0.0001);
-	pid4.setI(0);
-	pid4.setD(0);
-	pid4.setIZone(0);
-	pid4.setFF(0);
-	pid4.setOutputRange(min4, max4);
-	abs4.setPosition(0);
-} else if(numMotors > 2){
-	mc3.setNeutralMode(NeutralMode.Brake);
-	mc3.setInverted(true);
-	pid3.setP(0.0001);
-	pid3.setI(0);
-	pid3.setD(0);
-	pid3.setIZone(0);
-	pid3.setFF(0);
-	pid3.setOutputRange(min3, max3);
-	abs3.setPosition(0);
-} else if(numMotors > 1){
-	mc2.setNeutralMode(NeutralMode.Brake);
-	mc2.setInverted(true);
-	pid2.setP(0.0001);
-	pid2.setI(0);
-	pid2.setD(0);
-	pid2.setIZone(0);
-	pid2.setFF(0);
-	pid2.setOutputRange(min2, max2);
-	abs2.setPosition(0);
-} else if(numMotors > 0){
-	mc1.setNeutralMode(NeutralMode.Brake);
-	mc1.setInverted(true);
-	pid1.setP(0.0001);
-	pid1.setI(0);
-	pid1.setD(0);
-	pid1.setIZone(0);
-	pid1.setFF(0);
-	pid1.setOutputRange(min1, max1);
-	abs1.setPosition(0);
-}
+
+	public PID(String configName, double absEncoderOffset){
+			mc1 = RobotProvider.instance.getMotor(configName);
+			csm1 = (CANSparkMax)mc1;
+			pid1 = csm1.getPIDController();
+			abs1 = csm1.getAbsoluteEncoder(Type.kDutyCycle);
+			abs1.setZeroOffset(absEncoderOffset);
+			pid1.setFeedbackDevice(abs1);
+	}
+
+	public void setPIDF(double p, double i, double d, double ff){
+		pid1.setP(p);
+		pid1.setI(i);
+		pid1.setD(d);
+		pid1.setFF(ff);
+	}
+
+	public void setP(double p){
+		pid1.setP(p);
+	}
+
+	public void setI(double i){
+		pid1.setI(i);
+	}
+
+	public void setD(double d){
+		pid1.setD(d);
+	}
+
+	public void setFf(double ff){
+		pid1.setFF(ff);
+	}
+
+	public void setkDutyCycleAllowedClosedLoopError(double error){
+		pid1.setSmartMotionAllowedClosedLoopError(error, 0);
+	}
+
+	public void setDeadzone(double dz){
+		dz1 = dz;
+	}
+
+	public void setOutputRange(double min, double max){
+		maxspeed1 = max;
+		minspeed1 = min;
+		pid1.setOutputRange(min, max);
+	}
+
+	public void setMotorMode(NeutralMode mode){
+		mc1.setNeutralMode(mode);
+	}
+
+	public void setMaxMinLocation(double max, double min){
+		maxpos1 = max;
+		minpos1 = min;
+	}
+
+	public void setMaxVel(double max){
+		maxvel1 = max;
+		pid1.setSmartMotionMaxVelocity(max, 0);
+		pid1.setSmartMotionMinOutputVelocity(0, 0);
+	}
+
+	public void setMaxAccel(double max){
+		maxaccel1 = max;
+		pid1.setSmartMotionMaxAccel(max, 0);
+	}
 
 
-*/
+	public void magicallyGoToPositionUsingCLE(double position){
+		if(position > maxpos1){
+			position = maxpos1;
+		} else if(position < minpos1){
+			position = minpos1;
+		}
+
+		pid1.setReference(position, ControlType.kSmartMotion);
+	}
+
+	public void magicallyGoToPositionUsingNormalDZ(double position){
+		if(position > maxpos1){
+			position = maxpos1;
+		} else if(position < minpos1){
+			position = minpos1;
+		}
+
+		theState = PIDSTATE.PID;
+	}
+
+}
---
+++ src/main/java/com/team766/controllers/PIDException.java
@@ -0,0 +1,9 @@
+package com.team766.controllers;
+
+public class PIDException extends RuntimeException {
+	 
+		public PIDException(String errorMessage) {
+			super(errorMessage);
+		} 
+	
+}
