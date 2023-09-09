761553a22bb88bf9621e2e01ea3a2ec7067a9bd1
Max <>
2023-09-09 16:38:48 -0700

    ready for pr

--- src/main/java/com/team766/controllers/CanSparkMaxSmartMotionLinearPIDController.java
+++ src/main/java/com/team766/controllers/CanSparkMaxSmartMotionLinearPIDController.java
@@ -1,12 +1,19 @@
 public class CanSparkMaxSmartMotionLinearPIDController extends ExtendableCanSparkMaxSmartMotionPIDController {
 	
-	private double antigravPower;
+	private final double antigravPower;
 
 
 	public CanSparkMaxSmartMotionLinearPIDController(final String configName, final double absEncoderOffset, final double absEncoderOffsetForZeroEncoderUnits, final OffsetPoint first, final OffsetPoint second) {
 		super(configName, absEncoderOffset, absEncoderOffsetForZeroEncoderUnits, first, second);
 	}
 
+	public void setAntigravPower(double power){
+		antigravPower = power;
+	}
+
+	public void setNewSetpoint(double setPoint){
+		updateSetpointFromSubclass(setPoint);
+	}
 	
 
 	public void runPIDs(){
