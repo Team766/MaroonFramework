1ea1b5573871e13c046ef04ecd8c806bb2f40e59
TTVMixmix00 <68516760+TTVMixmix00@users.noreply.github.com>
2023-09-14 02:41:49 +0000

    fix more compile errors

--- src/main/java/com/team766/controllers/CanSparkMaxSmartMotionLinearPIDController.java
+++ src/main/java/com/team766/controllers/CanSparkMaxSmartMotionLinearPIDController.java
@@ -1,19 +1,15 @@
+package com.team766.controllers;
+
 public class CanSparkMaxSmartMotionLinearPIDController extends ExtendableCanSparkMaxSmartMotionPIDController {
 	
 	private final double antigravPower;
 
 
-	public CanSparkMaxSmartMotionLinearPIDController(final String configName, final double absEncoderOffset, final double absEncoderOffsetForZeroEncoderUnits, final OffsetPoint first, final OffsetPoint second) {
+	public CanSparkMaxSmartMotionLinearPIDController(final String configName, final double absEncoderOffset, final double absEncoderOffsetForZeroEncoderUnits, final OffsetPoint first, final OffsetPoint second, final double antigravityPower) {
 		super(configName, absEncoderOffset, absEncoderOffsetForZeroEncoderUnits, first, second);
+		antigravPower = antigravityPower;
 	}
 
-	public void setAntigravPower(double power){
-		antigravPower = power;
-	}
-
-	public void setNewSetpoint(double setPoint){
-		updateSetpointFromSubclass(setPoint);
-	}
 	
 
 	public void runPIDs(){
