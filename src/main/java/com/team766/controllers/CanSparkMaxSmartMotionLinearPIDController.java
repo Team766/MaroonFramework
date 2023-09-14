98dd22b4ed9679638d6c7b081e0b82b5bca35f38
TTVMixmix00 <68516760+TTVMixmix00@users.noreply.github.com>
2023-09-14 02:44:48 +0000

    fix spacing

--- src/main/java/com/team766/controllers/CanSparkMaxSmartMotionLinearPIDController.java
+++ src/main/java/com/team766/controllers/CanSparkMaxSmartMotionLinearPIDController.java
@@ -3,14 +3,11 @@ package com.team766.controllers;
 public class CanSparkMaxSmartMotionLinearPIDController extends ExtendableCanSparkMaxSmartMotionPIDController {
 	
 	private final double antigravPower;
-
-
+	
 	public CanSparkMaxSmartMotionLinearPIDController(final String configName, final double absEncoderOffset, final double absEncoderOffsetForZeroEncoderUnits, final OffsetPoint first, final OffsetPoint second, final double antigravityPower) {
 		super(configName, absEncoderOffset, absEncoderOffsetForZeroEncoderUnits, first, second);
 		antigravPower = antigravityPower;
 	}
-
-	
 
 	public void runPIDs(){
 		updateAntigrav(antigravPower);
