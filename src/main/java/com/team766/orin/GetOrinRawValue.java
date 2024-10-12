4cb9d6c1bbbeeea8ff036e8f690ddb9ad26ae281
Max Spier <68516760+Mixmix00@users.noreply.github.com>
2024-10-12 15:28:42 -0700

    apply linter

--- src/main/java/com/team766/orin/GetOrinRawValue.java
+++ src/main/java/com/team766/orin/GetOrinRawValue.java
@@ -1,19 +1,11 @@
 package com.team766.orin;
 
-import edu.wpi.first.networktables.BooleanEntry;
-import edu.wpi.first.networktables.BooleanTopic;
 import edu.wpi.first.networktables.DoubleArrayEntry;
 import edu.wpi.first.networktables.DoubleArrayTopic;
-import edu.wpi.first.networktables.DoubleEntry;
-import edu.wpi.first.networktables.DoubleTopic;
 import edu.wpi.first.networktables.IntegerArrayEntry;
 import edu.wpi.first.networktables.IntegerArrayTopic;
-import edu.wpi.first.networktables.IntegerEntry;
-import edu.wpi.first.networktables.IntegerTopic;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.networktables.StringEntry;
-import edu.wpi.first.networktables.StringTopic;
 
 /*
  * This class is used to get raw values from the NetworkTable. We can send values on the Orin to NetworkTables and get them easily here.
@@ -36,8 +28,8 @@ public class GetOrinRawValue {
     private static IntegerArrayTopic tagId = table.getIntegerArrayTopic("tag_id");
     private static long[] array1 = new long[] {0l};
     private static IntegerArrayEntry tagIdValues = tagId.getEntry(array1);
-    
-    public static double[] getRawPoseData() throws ValueNotFoundOnTableError{
+
+    public static double[] getRawPoseData() throws ValueNotFoundOnTableError {
         if (poseValues.get().length == 1 && poseValues.get()[0] == Double.NEGATIVE_INFINITY) {
             throw new ValueNotFoundOnTableError("Pose Data Not Present on Table");
         }
@@ -45,10 +37,11 @@ public class GetOrinRawValue {
         return poseValues.get();
     }
 
-    public static int[] getTagIds() throws ValueNotFoundOnTableError{
+    public static int[] getTagIds() throws ValueNotFoundOnTableError {
         long[] longValues = tagIdValues.get();
 
-        if (longValues.length == 0 || (longValues[0] == Integer.MIN_VALUE && longValues.length == 1)) {
+        if (longValues.length == 0
+                || (longValues[0] == Integer.MIN_VALUE && longValues.length == 1)) {
             throw new ValueNotFoundOnTableError("Tag ID Data Not Present on Table");
         }
 
@@ -59,83 +52,81 @@ public class GetOrinRawValue {
         return toReturn;
     }
 
-    public static void closeAll(){
+    public static void closeAll() {
         poseValues.close();
         tagIdValues.close();
     }
 }
 
+// Placeholder methods - can iterate on these as needed
+/*
+public static int getInt(String key) throws ValueNotFoundOnTableError {
+    IntegerTopic topic = table.getIntegerTopic(key);
+    IntegerEntry value = topic.getEntry(Integer.MIN_VALUE);
+    if (value.get() == Integer.MIN_VALUE) {
+        throw new ValueNotFoundOnTableError(key);
+    }
+    return (int) value.get();
+}
 
-//Placeholder methods - can iterate on these as needed
-/* 
-    public static int getInt(String key) throws ValueNotFoundOnTableError {
-        IntegerTopic topic = table.getIntegerTopic(key);
-        IntegerEntry value = topic.getEntry(Integer.MIN_VALUE);
-        if (value.get() == Integer.MIN_VALUE) {
-            throw new ValueNotFoundOnTableError(key);
-        }
-        return (int) value.get();
+public static double getDouble(String key) throws ValueNotFoundOnTableError {
+    DoubleTopic topic = table.getDoubleTopic(key);
+    DoubleEntry value = topic.getEntry(Double.NEGATIVE_INFINITY);
+    if (value.get() == Double.NEGATIVE_INFINITY) {
+        throw new ValueNotFoundOnTableError(key);
+    }
+    return value.get();
+}
+
+
+//Be careful to know that the boolean value is valid. If the value is not found, the default value is returned and no exception is thrown.
+//This is because true/false are the only two possible values for a boolean - if one were to be the default it would be hard to know if the value was not found or if the value was the default.
+
+public static boolean getBoolean(String key) {
+    BooleanTopic topic = table.getBooleanTopic(key);
+    BooleanEntry value = topic.getEntry(false);
+    return value.get();
+}
+
+public static String getString(String key) throws ValueNotFoundOnTableError {
+    StringTopic topic = table.getStringTopic(key);
+    StringEntry value =
+            topic.getEntry(
+                    "Team 766 is such a great team and they will go to worlds every year because they are such a great team!!!");
+    if (value.get()
+            .equals(
+                    "Team 766 is such a great team and they will go to worlds every year because they are such a great team!!!")) {
+        throw new ValueNotFoundOnTableError(key);
+    }
+    return value.get();
+}
+
+public static int[] getIntArray(String key) throws ValueNotFoundOnTableError {
+    IntegerArrayTopic topic = table.getIntegerArrayTopic(key);
+    long[] arr = new long[1];
+    arr[0] = Integer.MIN_VALUE;
+    IntegerArrayEntry values = topic.getEntry(arr);
+    long[] array = values.get();
+
+    if (array.length == 0 || (array[0] == Integer.MIN_VALUE && array.length == 1)) {
+        throw new ValueNotFoundOnTableError(key);
     }
 
-    public static double getDouble(String key) throws ValueNotFoundOnTableError {
-        DoubleTopic topic = table.getDoubleTopic(key);
-        DoubleEntry value = topic.getEntry(Double.NEGATIVE_INFINITY);
-        if (value.get() == Double.NEGATIVE_INFINITY) {
-            throw new ValueNotFoundOnTableError(key);
-        }
-        return value.get();
+    int[] toReturn = new int[array.length];
+    for (int i = 0; i < array.length; i++) {
+        toReturn[i] = (int) array[i];
+    }
+    return toReturn;
+}
+
+public static double[] getDoubleArray(String key) throws ValueNotFoundOnTableError {
+    DoubleArrayTopic topic = table.getDoubleArrayTopic(key);
+    double[] arr = new double[] {Double.NEGATIVE_INFINITY};
+    DoubleArrayEntry values = topic.getEntry(arr);
+    if (values.get().length == 1 && values.get()[0] == Double.NEGATIVE_INFINITY) {
+        throw new ValueNotFoundOnTableError(key);
     }
 
-    
-    //Be careful to know that the boolean value is valid. If the value is not found, the default value is returned and no exception is thrown.
-    //This is because true/false are the only two possible values for a boolean - if one were to be the default it would be hard to know if the value was not found or if the value was the default.
-     
-    public static boolean getBoolean(String key) {
-        BooleanTopic topic = table.getBooleanTopic(key);
-        BooleanEntry value = topic.getEntry(false);
-        return value.get();
-    }
-
-    public static String getString(String key) throws ValueNotFoundOnTableError {
-        StringTopic topic = table.getStringTopic(key);
-        StringEntry value =
-                topic.getEntry(
-                        "Team 766 is such a great team and they will go to worlds every year because they are such a great team!!!");
-        if (value.get()
-                .equals(
-                        "Team 766 is such a great team and they will go to worlds every year because they are such a great team!!!")) {
-            throw new ValueNotFoundOnTableError(key);
-        }
-        return value.get();
-    }
-
-    public static int[] getIntArray(String key) throws ValueNotFoundOnTableError {
-        IntegerArrayTopic topic = table.getIntegerArrayTopic(key);
-        long[] arr = new long[1];
-        arr[0] = Integer.MIN_VALUE;
-        IntegerArrayEntry values = topic.getEntry(arr);
-        long[] array = values.get();
-
-        if (array.length == 0 || (array[0] == Integer.MIN_VALUE && array.length == 1)) {
-            throw new ValueNotFoundOnTableError(key);
-        }
-
-        int[] toReturn = new int[array.length];
-        for (int i = 0; i < array.length; i++) {
-            toReturn[i] = (int) array[i];
-        }
-        return toReturn;
-    }
-
-    public static double[] getDoubleArray(String key) throws ValueNotFoundOnTableError {
-        DoubleArrayTopic topic = table.getDoubleArrayTopic(key);
-        double[] arr = new double[] {Double.NEGATIVE_INFINITY};
-        DoubleArrayEntry values = topic.getEntry(arr);
-        if (values.get().length == 1 && values.get()[0] == Double.NEGATIVE_INFINITY) {
-            throw new ValueNotFoundOnTableError(key);
-        }
-
-        return values.get();
-    }
-    */
-
+    return values.get();
+}
+*/
